fbde8fb28dc5f86cf31a9aaac17dabd8ade93aa3
Angel Evtimov <acho1992@abv.bg>
2015-08-31 19:45:34 +0300

    Added comments to some of the methods

--- DoublyLinkedList/DoublyLinkedList.cs
+++ DoublyLinkedList/DoublyLinkedList.cs
@@ -91,43 +91,67 @@ namespace DoublyLinkedList
         {
             if (index < 0 || index > count)
                 throw new ArgumentOutOfRangeException("Index is out of range");
-            Node newNode = new Node(item);
+            Node newNode = new Node(item); // syzdavame elementa, kojto shte vmykvame
             int currentIndex = 0;
-            Node current = head;
-            Node prev = null;
+            Node current = head; 
+            Node prev = null; // tazi promenliva pazi predishniq element. Principno moje da se maxne ottuk(kakto i dolu v while-a) i da se zameni s current.Prev navsqkude kydeto se sreshta
+                              // po-dolu. Tova moje da se napravi samo pri dvusvyzaniq spisuk zashtoto pri ednosvyrzaniq nqmame Prev, a samo Next
             while (currentIndex < index)
             {
-                prev = current;
-                current = current.Next;
-                currentIndex++;
+                prev = current; // primerno ako imame spisyk s elementi 1 2 3 i iskame da vmyknem element na poziciq 1(mejdu 1 i 2) prev shte e raven na 1
+                current = current.Next; // current shte e raven na 2
+                currentIndex++; // currentIndex shte e raven na 1
             }
-            if (index == 0)
+            if (index == 0) // ako poziciqta na koqto iskame da dobavim element e 0 (t.e predi vsichki elementi)
             {
-                if (count == 0)
+                if (count == 0) // proverqvame dali spisyka e prazen, ako da dobavqme nov element izpolzvajki metoda napisan po-gore(Add) i s return prekratqvame izpylnenieto na InsertAt
                 {
                     this.Add(item);
                     return;
                 }
-                else
+                else // ako spisyka ne e bil prazen, a poziiciqta na koqto iskame da dobavim element e 0(t.e. predi vsichki elementi) vlizame tuk
                 {
-                    newNode.Prev = head.Prev;
-                    newNode.Next = head;
-                    head.Prev = newNode;
-                    head = newNode;
+                    /*  newNode = 4, head = 1
+                 *     head.Prev      |             (1)                 (3)     head  |                (4)
+                 *         |          |              |                   |       |    |                 |
+                 *   null <--  1      |      null <-------- newNode <--------   1     |  null <----- newNode <------- 1
+                 *             |      |                         4    -------->        |                 4     ------->
+                 *           head     |                                  |            |
+                 *                    |                                 (2)           |
+                 */
+                    newNode.Prev = head.Prev; // (1)
+                    newNode.Next = head;      // (2)
+                    head.Prev = newNode;      // (3)
+                    head = newNode;           // (4)
                 }
             }
-            else if (index == count)
-            {
-                newNode.Prev = tail;
-                tail.Next = newNode;
-                tail = newNode;
+            else if (index == count) // ako poziciqta na koqto iskame da dobavim element e ravna na broq na elementite(t.e sled vsichki elementi) vlizame tuk 
+            {   /*  newNode = 4, tail = 3
+                 *     tail            |       tail newNode.Prev  |                tail
+                 *       |             |         |     |          |                 |
+                 *       3 --> null    |         3 <---- newNode  |        3 <-- newNode --> null 
+                 *         |           |            ---->   4     |           -->    4
+                 *       tail.Next     |              |           |
+                 *                     |          tail.Next       |
+                 */
+                newNode.Prev = tail; 
+                tail.Next = newNode; 
+                tail = newNode; 
             }
-            else
+            else // ako poziciqta e mejdu elementite v spisyka(t.e. nito v nachaloto nito v kraq) vlizame tuk
             {
-                newNode.Prev = current.Prev;
-                current.Prev = newNode;
-                newNode.Next = prev.Next;
-                prev.Next = newNode;
+                /*  newNode = 4, prev = 1, current = 2
+                 *     current.Prev      |               (1)               (2)    
+                 *         |             |                |                 |
+                 *   prev <-- current    |      prev  <-------- newNode <--------  current
+                 *     1  -->    2       |       1    -------->    4    -------->     2
+                 *         |             |                |                 |
+                 *       prev.Next       |               (4)               (3) 
+                 */
+                newNode.Prev = current.Prev; // (1)
+                current.Prev = newNode;      // (2)
+                newNode.Next = prev.Next;    // (3) newNode.Next = current.Prev.Next \___ pravqt sushtoto
+                prev.Next = newNode;         // (4) current.Prev.Next = newNode      /
             }
             count++;
         }
@@ -145,33 +169,54 @@ namespace DoublyLinkedList
                 current = current.Next;
                 currentIndex++;
             }
-            if (index == 0)
+            if (index == 0) // ako poziciqta na koqto iskame da premaxmen element e 0 (t.e 1viq element)
             {
-                if (count == 1)
+                if (count == 1) // ako broq na elementite e raven na 1(toest sled premahvane na elementa na nuleva poziciq spisyka stava prazen)
                 {
+                    // pravim glavata i opashkata null
                     head = null;
                     tail = null;
                 }
-                else
+                else // ako ne vlizame tuk
                 {
-                    head = head.Next;
+                    // premestvame glavata na sledvashtiq element i pravim Prev pointera mu da sochi kym null
+                    head = current.Next;
+                    head.Prev = null;
                 }
             }
-            else if (index == count - 1)
+            else if (index == count - 1) // ako poziciqta e v kraq na spisuka(t.e. ako imame 2 elementa, 2riq element se namira na indeks 1, koeto e count-1) vlizame tyk
             {
-                prev.Next = current.Next;
-                tail = prev;
-                current = null;
+                /*  current = 2, prev = 3, tail = 2
+                *             tail      |     tail = prev                          
+                *               |       |        |                          
+                *   prev <-- current    |      prev  <-----current-------->  null 
+                *     3  -->    2       |       3   \         2             /    
+                *         |             |            \_____________________/                    
+                *         |             |                      |      
+                *       prev.Next       |             prev.Next = current.Next            
+                */
+                prev.Next = current.Next; // prev.Next = null - pravi sushtoto
+                tail = prev;                       
             }
-            else
+            else // ako poziciqta e mejdu elementite v spisyka(t.e. nito v nachaloto nito v kraq) vlizame tuk
             {
+                /*  prev = 2, current = 3, current.Next = 4
+                *      current.Prev     |              current.Next.Prev = prev;                                     
+                *         |             |           ___________|_____________ 
+                *         |             |          /                         \
+                *   prev <-- current    |      prev  <-----current       current.Next
+                *     2  -->    3       |       2  \         3     -------->  4 
+                *         |             |           \________________________/                    
+                *         |             |                      |      
+                *       prev.Next       |           prev.Next = current.Next            
+                */
                 prev.Next = current.Next;
                 current.Next.Prev = prev;
             }
             count--;
         }
 
-        /*
+        /* - Alternativna versiq na metoda Remove napisan po-dolu, izpolzvaiki metoda RemoveAt, napisan po-gore
         public void Remove(object item)
         {
             int currentIndex = 0;
@@ -190,12 +235,13 @@ namespace DoublyLinkedList
         }
         */
 
-        public void Remove(object item)
+        public void Remove(object item) /*Metod za premahvane na obekt po st-st. Povecheto neshta sa vzaimstvani ot metoda RemoveAt, zatova spored men zakomentiraniq metod Remove 
+            po-gore e po-udachniq variant*/
         {
             Node current = head;
             Node prev = null;
-            int itemIndex = 0;
-            while (!current.Data.Equals(item))
+            int itemIndex = 0; 
+            while (!current.Data.Equals(item)) // Neshtata v while-a se izpylnqvat dokato stoinostta na segashniq element e razlichna ot tazi na elementa kojto iskame da premahnem 
             {
                 prev = current;
                 current = current.Next;
@@ -210,14 +256,14 @@ namespace DoublyLinkedList
                 }
                 else
                 {
-                    head = head.Next;
+                    head = current.Next;
+                    head.Prev = null;
                 }
             }
             else if (itemIndex == count-1)
             {
                 prev.Next = current.Next;
                 tail = prev;
-                current = null;
             }
             else
             {
@@ -322,7 +368,7 @@ namespace DoublyLinkedList
             test.Print();
             Console.WriteLine("List elements after changing element with value '2' to '3':");
             test.ChangeElementData(2, 3);
-            test.Print();                 
+            test.Print();
             Console.WriteLine("List elements after removing element with value '5'");
             test.Remove(4);
             test.Print();
