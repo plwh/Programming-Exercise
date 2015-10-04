3c1afa9714be13cd6373d0b1afb2065f7dfdec6b
Angel Evtimov <acho1992@abv.bg>
2015-10-04 21:44:15 +0300

    Updates
    Made some changes.

--- BasicPongGame/Form1.Designer.cs
+++ BasicPongGame/Form1.Designer.cs
@@ -28,20 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "score:0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(717, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "score:0";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(189, 114);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(383, 99);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Game Over";
+            this.textBox1.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(185, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(402, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Press Spacebar to restart the game. Press Enter to exit.";
+            this.label3.Visible = false;
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(784, 562);
+            this.shapeContainer1.TabIndex = 4;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.SystemColors.ButtonFace;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 391;
+            this.lineShape1.X2 = 392;
+            this.lineShape1.Y1 = 3;
+            this.lineShape1.Y2 = 558;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 398);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeContainer1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
     }
 }
 
--- BasicPongGame/Form1.cs
+++ BasicPongGame/Form1.cs
@@ -25,11 +25,15 @@ namespace BasicPongGame
         int ballSpeedX = 6;
         int ballSpeedY = 6;
         int gameTimeInterval = 1;
+        int playerPoints = 0;
+        int computerPoints = 0;
+        int aiSpeed = 0;
+        Random setSpeed = new Random();
 
         public Form1()
         {
             InitializeComponent();
-
+            
             picBoxPlayer = new PictureBox();
             picBoxAI = new PictureBox();
             picBoxBall = new PictureBox();
@@ -47,17 +51,17 @@ namespace BasicPongGame
 
             picBoxPlayer.Size = sizePlayer;
             picBoxPlayer.Location = new Point(picBoxPlayer.Width / 2, ClientSize.Height / 2 - picBoxPlayer.Height / 2);
-            picBoxPlayer.BackColor = Color.Blue;
+            picBoxPlayer.BackColor = Color.White;
             this.Controls.Add(picBoxPlayer);
 
             picBoxAI.Size = sizeAI;
             picBoxAI.Location = new Point(ClientSize.Width - (picBoxAI.Width + picBoxAI.Width / 2), ClientSize.Height / 2 - picBoxPlayer.Height / 2);
-            picBoxAI.BackColor = Color.Red;
+            picBoxAI.BackColor = Color.White;
             this.Controls.Add(picBoxAI);
 
             picBoxBall.Size = sizeBall;
             picBoxBall.Location = new Point(ClientSize.Width / 2 - picBoxBall.Width / 2, ClientSize.Height / 2 - picBoxBall.Height / 2);
-            picBoxBall.BackColor = Color.Green;
+            picBoxBall.BackColor = Color.White;
             this.Controls.Add(picBoxBall);
         }
 
@@ -67,14 +71,40 @@ namespace BasicPongGame
             gameAreaCollisions();
             paddleCollision();
             playerMovement();
-            aiMovement();
+            aiMovement(aiSpeed);
         }
 
-        private void aiMovement()
+        private void updateScore()
+        {
+            if (picBoxBall.Location.X > ClientSize.Width)
+            {
+                playerPoints++;
+                label1.Text = "score:" + playerPoints.ToString();
+            }  
+            else if (picBoxBall.Location.X < 0)
+            {
+                computerPoints++;
+                label2.Text = "score:" + computerPoints.ToString();
+            }       
+        }
+
+        public void restartGame()
+        {
+            playerPoints = 0;
+            computerPoints = 0;
+            label1.Text = "score:" + playerPoints.ToString();
+            label2.Text = "score:" + computerPoints.ToString();
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox1.Visible = false;
+            label3.Visible = false;
+        }
+
+        private void aiMovement(int currentSpeed)
         {
             if (ballSpeedX > 0)
             {
-                picBoxAI.Location = new Point(ClientSize.Width - (picBoxAI.Width + picBoxAI.Width / 2), picBoxBall.Location.Y - picBoxAI.Height / 2);
+                picBoxAI.Location = new Point(ClientSize.Width - (picBoxAI.Width + picBoxAI.Width / 5), (picBoxBall.Location.Y - picBoxAI.Height / 2) + currentSpeed);
             }
         }
 
@@ -91,33 +121,72 @@ namespace BasicPongGame
 
         private void paddleCollision()
         {
-            if (picBoxBall.Bounds.IntersectsWith(picBoxAI.Bounds) || picBoxBall.Bounds.IntersectsWith(picBoxPlayer.Bounds))
+            if (picBoxBall.Bounds.IntersectsWith(picBoxAI.Bounds))
             {
                 ballSpeedX = -ballSpeedX;
             }
+            else if(picBoxBall.Bounds.IntersectsWith(picBoxPlayer.Bounds))
+            {
+                changeAISpeed();
+                ballSpeedX = -ballSpeedX;          
+            }
+        }
+
+        private void changeAISpeed()
+        {
+            aiSpeed = setSpeed.Next(60,66);
         }
 
         private void gameAreaCollisions()
         {
             if (picBoxBall.Location.Y > ClientSize.Height - picBoxBall.Height || picBoxBall.Location.Y < 0)
             {
-                ballSpeedY = -ballSpeedY;
+                ballSpeedY = -ballSpeedY; 
             }
-            else if (picBoxBall.Location.X > ClientSize.Width)
+            if ((picBoxBall.Location.X > ClientSize.Width || picBoxBall.Location.X < 0) && checkForWinner() != true)
             {
-                resetBall();
-            }
-            else if (picBoxBall.Location.X < 0)
-            {
-                resetBall();
+                updateScore();
+                changeAISpeed();
+                resetBall(true);
             }
         }
 
-        private void resetBall()
+        private bool checkForWinner()
         {
-            picBoxBall.Location = new Point(ClientSize.Width / 2 - picBoxBall.Width / 2, ClientSize.Height / 2 - picBoxBall.Height / 2);
+            if (playerPoints == 9 || computerPoints == 9)
+            { 
+                label1.Visible = false;
+                label2.Visible = false;
+                lineShape1.Visible = false;
+                textBox1.Visible = true;
+                label3.Visible = true;
+                aiSpeed = 0;
+                resetBall(false);
+                return true;
+            }
+            return false;
         }
 
-        
+        public void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Space && checkForWinner() == true)
+            {
+                restartGame();
+                resetBall(true);
+            }
+            else if (e.KeyChar == (char)Keys.Enter && checkForWinner() == true)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void resetBall(bool a)
+        {
+            if (a == true)
+            {
+                picBoxBall.Location = new Point(ClientSize.Width / 2 - picBoxBall.Width / 2, ClientSize.Height / 2 - picBoxBall.Height / 2);
+                ballSpeedX = -ballSpeedX;
+            }
+        }  
     }
 }
